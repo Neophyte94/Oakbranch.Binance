8d8725ea3f3bda7d6532016cc21af893ff938410
Neophyte94 <guerrero940409@gmail.com>
2023-10-27 01:45:01 +0300

    Added the 'NormalizeSymbol()' method, improved XML comments.

--- Oakbranch.Binance/CommonUtility.cs
+++ Oakbranch.Binance/CommonUtility.cs
@@ -32,30 +32,64 @@ namespace Oakbranch.Binance
 
         #region Static methods
 
-        public static int Clamp(int value, int min, int max)
+        /// <summary>
+        /// Normalizes the given string representing an exchange symbol to the format accepted by the Binance API.
+        /// </summary>
+        /// <param name="symbol">The exchange symbol to be normalized.</param>
+        /// <returns>The exchange symbol adapted to the format accepted by the exchange API.</returns>
+        public static string NormalizeSymbol(string symbol)
         {
-            if (value < min) return min;
-            else if (value > max) return max;
-            else return value;
+            return symbol?.ToUpperInvariant();
         }
 
-        public static string DecodeByteContent(byte[] content)
-        {
-            if (content == null || content.Length == 0) return null;
-            try { return Encoding.ASCII.GetString(content); }
-            catch { return $"{{ {String.Join(", ", content.Select((b) => b.ToString("2X")))} }}"; }
-        }
-
+        /// <summary>
+        /// Converts the <see cref="long"/> value representing time in the Binance API format
+        /// to a corresponding <see cref="DateTime"/> instance.
+        /// </summary>
+        /// <param name="ms">The time value in the Binance API format to convert.</param>
+        /// <returns>A <see cref="DateTime"/> instance corresponding to the specified time.</returns>
         public static DateTime ConvertToDateTime(long ms)
         {
             return s_ApiBaseDate + TimeSpan.FromMilliseconds(ms);
         }
 
+        /// <summary>
+        /// Converts the given date &amp; time to a <see cref="long"/> value in the Binance API format.
+        /// </summary>
+        /// <param name="value">The date &amp; time to convert.</param>
+        /// <returns>A <see cref="long"/> value corresponding to the specified time in the Binance API format.</returns>
         public static long ConvertToApiTime(DateTime value)
         {
             return (long)(value - ApiBaseDate).TotalMilliseconds;
         }
 
+        /// <summary>
+        /// Decodes a byte array content to a string, using ASCII encoding.
+        /// <para>If decoding fails, returns a formatted string of the HEX representation of the byte array.</para>
+        /// </summary>
+        /// <param name="content">The byte array content to be decode.</param>
+        /// <returns>A string representing the decoded content, or the HEX representation of the byte content.</returns>
+        public static string DecodeByteContent(byte[] content)
+        {
+            if (content == null || content.Length == 0)
+            {
+                return null;
+            }
+            try
+            {
+                return Encoding.ASCII.GetString(content);
+            }
+            catch
+            {
+                return $"{{ {String.Join(", ", content.Select((b) => b.ToString("2X")))} }}";
+            }
+        }
+
+        /// <summary>
+        /// Finds the index of the first empty or whitespace string within the given array.
+        /// </summary>
+        /// <param name="items">The array of strings to search through.</param>
+        /// <returns>The index of the first empty or whitespace string, or -1 if none exists.</returns>
         public static int IndexOfEmpty(string[] items)
         {
             if (items == null) return -1;
@@ -71,6 +105,12 @@ namespace Oakbranch.Binance
             return -1;
         }
 
+        /// <summary>
+        /// Creates a <see cref="TimeSpan"/> instance based on the specified interval.
+        /// </summary>
+        /// <param name="interval">The interval type to create a timespan from.</param>
+        /// <param name="num">The number of intervals to create a timespan from.</param>
+        /// <returns>A <see cref="TimeSpan"/> instance representing the specified interval.</returns>
         public static TimeSpan CreateTimespan(Interval interval, int num)
         {
             switch (interval)
@@ -88,6 +128,11 @@ namespace Oakbranch.Binance
             }
         }
 
+        /// <summary>
+        /// Gets a natural-language description of the given timespan.
+        /// </summary>
+        /// <param name="interval">The timespan to describe.</param>
+        /// <returns>A string representing the duration of the given timespan (in terms of days, hours, etc).</returns>
         public static string GetIntervalDescription(TimeSpan interval)
         {
             if (interval.Ticks >= TimeSpan.TicksPerDay)
