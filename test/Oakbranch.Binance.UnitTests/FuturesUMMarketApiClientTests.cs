fa98012eb04e5a7eadeb8a5b6e87820db33c9010
Neophyte94 <guerrero940409@gmail.com>
2023-11-25 23:25:38 +0200

    Added tests for the 'PreapreGetOldTrades' method.

--- test/Oakbranch.Binance.UnitTests/FuturesUMMarketApiClientTests.cs
+++ test/Oakbranch.Binance.UnitTests/FuturesUMMarketApiClientTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Oakbranch.Binance.Abstractions;
 using Oakbranch.Binance.Clients;
 using Oakbranch.Binance.Core.RateLimits;
+using Oakbranch.Binance.Models;
 using Oakbranch.Binance.Models.Futures;
 
 namespace Oakbranch.Binance.UnitTests;
@@ -18,6 +19,19 @@ public class FuturesUMMarketApiClientTests : ApiClientTestsBase
 
     #region Static members
 
+    public static object[] CorrectQueryPeriodCases { get; } = new object[]
+    {
+        new object?[] { ReferenceDateTime.AddHours(-1.0), null },
+        new object?[] { ReferenceDateTime.AddDays(-1.0).AddHours(-4.0), ReferenceDateTime.AddDays(-1.0) },
+        new object?[] { null, ReferenceDateTime.AddDays(-1.0) },
+        new object?[] { ReferenceDateTime.AddYears(-1), null },
+        new object?[] { ReferenceDateTime.AddHours(-1.0).AddSeconds(-1.0), ReferenceDateTime.AddHours(-1.0) }
+    };
+    public static object[] InvalidQueryPeriodCases { get; } = new object[]
+    {
+        new object?[] { ReferenceDateTime.AddHours(-1.0), ReferenceDateTime.AddHours(-2.0), },
+        new object?[] { ReferenceDateTime, ReferenceDateTime.AddSeconds(-1.0), },
+    };
     public static object[] AllKlineIntervalCases { get; } = Enum.GetValues<KlineInterval>()
         .Select((i) =>
         {
@@ -38,7 +52,7 @@ public class FuturesUMMarketApiClientTests : ApiClientTestsBase
     #region Instance constructors
 
     public FuturesUMMarketApiClientTests(bool areResultsLogged)
-        : base(CreateDefaultLogger<SpotMarketApiClientTests>(LogLevel.Information), areResultsLogged)
+        : base(CreateDefaultLogger<FuturesUMMarketApiClientTests>(LogLevel.Information), areResultsLogged)
     {
         if (!ApiConnectorSource.TryReadApiKeysFromContainer(out string apiKey, out string? secretKey))
         {
@@ -104,7 +118,7 @@ public class FuturesUMMarketApiClientTests : ApiClientTestsBase
         }
     }
 
-    // Check server time tests.
+    // Check server time.
     [Test, Retry(DefaultTestRetryLimit)]
     public async Task CheckServerTime_ReturnsValidTime_WhenDefaultParams()
     {
@@ -124,7 +138,7 @@ public class FuturesUMMarketApiClientTests : ApiClientTestsBase
         }
     }
 
-    // Get exchange info tests.
+    // Get exchange info.
     [Test, Retry(DefaultTestRetryLimit)]
     public async Task GetExchangeInfo_ReturnsValidInstance_WhenDefaultParams()
     {
@@ -141,14 +155,140 @@ public class FuturesUMMarketApiClientTests : ApiClientTestsBase
         {
             Assert.That(result.Symbols, Is.Not.Null.And.Count.GreaterThan(0));
             Assert.That(result.Assets, Is.Not.Null.And.Count.GreaterThan(0));
-            Assert.That(result.ServerTime, Is.EqualTo(DateTime.UtcNow).Within(UtcTimeErrorTolerance));
             Assert.That(result.Timezone, Is.Not.Null);
         });
 
         if (AreQueryResultsLogged)
         {
             LogObject(result);
+            LogCollection(result.Symbols);
+            LogCollection(result.Assets);
         }
+    }
+
+    // Get old trades.
+    [Test, Retry(DefaultTestRetryLimit)]
+    public async Task GetOldTrades_ReturnsDefaultCount_WhenOnlySymbolSpecified()
+    {
+        // Arrange.
+        List<Trade> result;
+
+        // Act;
+        using IDeferredQuery<List<Trade>> query = _client.PrepareGetOldTrades(DefaultSymbol);
+        result = await query.ExecuteAsync(CancellationToken.None).ConfigureAwait(false);
+
+        // Assert.
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Has.Count.EqualTo(FuturesUMMarketApiClient.DefaultTradesQueryLimit));
+
+        if (AreQueryResultsLogged)
+        {
+            LogCollection(result, 10);
+        }
+    }
+
+    [Retry(DefaultTestRetryLimit)]
+    [TestCase(1)]
+    [TestCase(FuturesUMMarketApiClient.MaxTradesQueryLimit - 1)]
+    [TestCase(FuturesUMMarketApiClient.MaxTradesQueryLimit)]
+    public async Task GetOldTrades_ReturnsExactCount_WhenSymbolAndLimitSpecified(int limit)
+    {
+        // Arrange.
+        List<Trade> result;
+
+        // Act.
+        using IDeferredQuery<List<Trade>> query = _client.PrepareGetOldTrades(DefaultSymbol, limit: limit);
+        result = await query.ExecuteAsync(CancellationToken.None).ConfigureAwait(false);
+
+        // Assert.
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Has.Count.EqualTo(limit));
+
+        if (AreQueryResultsLogged)
+        {
+            LogCollection(result, 10);
+        }
+    }
+
+    [TestCaseSource(nameof(NullAndWhitespaceStringCases))]
+    public void GetOldTrades_ThrowsArgumentNullException_WhenEmptySymbolSpecified(string symbol)
+    {
+        // Arrange.
+        TestDelegate td = new TestDelegate(() => _client.PrepareGetOldTrades(symbol));
+
+        // Act & Assert.
+        Assert.That(td, Throws.ArgumentNullException);
+    }
+
+    // Get aggregate trades.
+    [TestCaseSource(nameof(CorrectQueryPeriodCases)), Retry(DefaultTestRetryLimit)]
+    public async Task GetAggregateTrades_ReturnsItemsWithinPeriod_WhenPeriodSpecified(DateTime? from, DateTime? to)
+    {
+        // Arrange.
+        List<AggregateTrade> result;
+
+        // Act.
+        using IDeferredQuery<List<AggregateTrade>> query = _client.PrepareGetAggregateTrades(
+            symbol: DefaultSymbol,
+            startTime: from,
+            endTime: to,
+            limit: null);
+        result = await query.ExecuteAsync(CancellationToken.None).ConfigureAwait(false);
+
+        // Assert.
+        Assert.That(result, Is.Not.Null);
+        if (from != null)
+        {
+            Assert.That(result.Select((at) => at.Timestamp), Has.All.GreaterThanOrEqualTo(from.Value));
+        }
+        if (to != null)
+        {
+            Assert.That(result.Select((at) => at.Timestamp), Has.All.LessThanOrEqualTo(to.Value));
+        }
+
+        if (AreQueryResultsLogged)
+        {
+            LogCollection(result, 10);
+        }
+    }
+
+    [TestCaseSource(nameof(NullAndWhitespaceStringCases))]
+    public void GetAggregateTrades_ThrowsArgumentNullException_WhenEmptySymbolSpecified(string symbol)
+    {
+        // Arrange.
+        TestDelegate td = new TestDelegate(() => _client.PrepareGetAggregateTrades(symbol));
+
+        // Act & Assert.
+        Assert.That(td, Throws.ArgumentNullException);
+    }
+
+    [TestCaseSource(nameof(InvalidQueryPeriodCases))]
+    public void GetAggregateTrades_ThrowsArgumentException_WhenInvalidPeriodSpecified(DateTime from, DateTime to)
+    {
+        // Arrange.
+        TestDelegate td = new TestDelegate(() =>
+            _client.PrepareGetAggregateTrades(
+                symbol: DefaultSymbol,
+                startTime: from,
+                endTime: to));
+
+        // Act & Assert.
+        Assert.That(td, Throws.ArgumentException);
+    }
+
+    [TestCase(0)]
+    [TestCase(-1)]
+    [TestCase(FuturesUMMarketApiClient.MaxTradesQueryLimit + 1)]
+    public void GetAggregateTrades_ThrowsArgumentOutOfRangeException_WhenInvalidLimitSpecified(int limit)
+    {
+        // Arrange.
+        TestDelegate td = new TestDelegate(() =>
+            _client.PrepareGetAggregateTrades(
+                symbol: DefaultSymbol,
+                limit: limit));
+
+        // Act & Assert.
+        Assert.That(td, Throws.Exception.AssignableFrom<ArgumentOutOfRangeException>());
     }
 
     #endregion
